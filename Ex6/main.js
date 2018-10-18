3b84d99095d9aee599410905dbbd49cdd2e47b7b
IsaacDdR <isaacovic00@gmail.com>
2018-10-18 11:46:27 -0500

    Random story end

--- Ex6/main.js
+++ Ex6/main.js
@@ -6,41 +6,60 @@ var story = document.querySelector('.story');
 function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
 }
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+var insertX = [
+'Willy the Goblin',
+'Big Daddy',
+'Father Christmas'];
 
-the soup kitchen
-Disneyland
-the White House
+var insertY = [
+'the soup kitchen',
+'Disneyland',
+'the White House'];
 
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+var insertZ = [
+'spontaneously combusted',
+'melted into a puddle on the sidewalk',
+'turned into a slug and crawled away'];
+
 randomize.addEventListener('click', result);
 
 function result() {
 
+  var newStory = storyText;
+
+  var xItem = randomValueFromArray(insertX);
+
+  var yItem = randomValueFromArray(insertY);
+
+  var zItem = randomValueFromArray(insertZ);
+
+  newStory  = newStory.replace(':insertx:' , xItem);
+
+  newStory = newStory.replace(':insertx' , xItem)
+
+  newStory = newStory.replace(':inserty:' , yItem);
+
+  newStory = newStory.replace(':insertz:', zItem);
+
   if(customName.value !== '') {
     var name = customName.value;
+
+    newStory = newStory.replace('Bob', name);
 
   }
 
   if(document.getElementById("uk").checked) {
-    var weight = Math.round(300);
-    var temperature =  Math.round(94);
-
+    var weight = Math.round(300* 0.0714286) + ' stone';
+    var temperature =  Math.round((94 - 32) * 5 / 9) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
+
+
+
 }
-var newStory = storyText;
-
-var xItem = randomValueFromArray(insertx);
-
-var yItem = randomValueFromArray(inserty);
-
-var zItem = randomValueFromArray(insertz);
